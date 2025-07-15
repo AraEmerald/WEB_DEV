9e411b302b85a244d73173dad4f11b3a53e529f1
Ara <araemerald28@gmail.com>
2025-07-15 12:02:27 +0530

    15July

--- 14July2025.js
+++ 14July2025.js
@@ -1,5 +1,7 @@
-<<<<<<< HEAD
 // DEBOUNCING
+
+// to give delay for a function to execute 
+// e.g. to make search bar
 
 // technique to make sure that a function is not called to many times in a short period of time.
 // used to prevent function from being called multiple times when user is typing in a textbox or clicking a button.
@@ -32,38 +34,3 @@ searchinput.addEventListener("input",(event)=>{
 })
 
 
-=======
-// DEBOUNCING
-
-// technique to make sure that a function is not called to many times in a short period of time.
-// used to prevent function from being called multiple times when user is typing in a textbox or clicking a button.
-
-// controlling number of calls to optimise solution. basically making optimistic calls to invoke function.
-
-
-// // search letter by letter
-
-// console.log("SCRIPT START");
-
-// const searchinput = document.getElementById("searchInput");
-
-// searchinput.addEventListener("input",(event)=>{
-//     console.log("INPUT WORD: ",event.target.value)
-// })
-
-
-// // take the whole word as input
-
-console.log("SCRIPT START");
-
-const searchinput = document.getElementById("searchInput");
-
-searchinput.addEventListener("input",(event)=>{
-    clearTimeout(timeout);
-    timeout = setTimeout(()=>{
-        console.log("INPUT WORD: ",event.target.value)
-    },2000);   
-})
-
-
->>>>>>> 1451b225595424d7b63571a7f51d2752a735f78d
---
+++ 15July2025.js
@@ -0,0 +1,15 @@
+// NODE JS
+
+// open-source and cross-platform JavaScript runtime environment
+// runs the V8 JavaScript engine
+// runs a single process. doesnt create a new thread everytime. single threaded.
+// asynchronous event driven architecture
+
+// CALLBACK QUEUE - micro and macro 
+// micro has higher priorty
+
+// micro task = promise, async 
+// macro task = setTimeout, setInterval
+
+
+
