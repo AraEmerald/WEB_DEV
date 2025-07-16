1eca102db7ace2b48b66734d5580fe0788f39457
Ara <araemerald28@gmail.com>
2025-07-16 14:22:00 +0530

    16 july

---
+++ 16July2025/index.js
@@ -0,0 +1,27 @@
+// setTimeout()         delay - event
+// setInterval()        delay - event repeat
+// setImmediate()       callback
+// process.next.tick
+
+const https = require('node:https');
+const fs = require('node:fs');
+
+setTimeout(()=>{
+    console.log("THIS ST3")
+},10)
+
+fs.readFile('./something.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
+
+var a =200;
+
+setTimeout(()=>{
+console.log("THIS ST1")
+},0)
+
+
